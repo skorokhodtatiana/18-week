0224b0836d8d4781378b188dc28fd3d9edadfc08
skorokhodtatiana <tanjakharza@mail.ru>
2021-08-31 22:26:49 +0300

    layout done, html5 validation done

---
+++ 1-task-validation-html5/validationHTML5.js
@@ -0,0 +1,5 @@
+
+    let buttonSubmit = document.getElementById('buttonSubmit');
+    buttonSubmit.addEventListener('submit', function preventSubmit(event){
+        event.preventDefault();
+    })
