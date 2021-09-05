75d39d8e5f8f09f9c3aef29a4ac9c4202d139a65
skorokhodtatiana <tanjakharza@mail.ru>
2021-09-05 17:51:42 +0300

    done this task

---
+++ 4-task-notes/notes.js
@@ -0,0 +1,37 @@
+const buttonSubmit = document.getElementById('buttonSubmit');
+const textNotes = document.getElementById('textNotes');
+const fieldForDate = document.getElementById('fieldForDate');
+const titleNote = document.getElementById('titleNote');
+
+function showDate() {
+    let newDate = new Date();
+    let formatNewDate = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate(), newDate.getHours(), newDate.getMinutes());
+    if (newDate.getMonth() < 10) {
+        monthFormat = ("0" + (Number(newDate.getMonth()) + +1));
+    }
+
+    if (newDate.getDate() < 10) {
+        dateFormat = ("0" + newDate.getDate());
+    }
+    fieldForDate.innerHTML = dateFormat + "." + monthFormat + "." + newDate.getFullYear() + " " + newDate.getHours() + ":" + newDate.getMinutes();
+    return formatNewDate;
+}
+
+function setNotes() {
+    let titleNewNote = titleNote.value;
+    let textNewNote = textNotes.value;
+     localStorage.setItem(titleNewNote, textNewNote); 
+}
+
+function cleanForm() {
+    titleNote.value = "";
+    textNotes.value = "";
+}
+
+buttonSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
+    setNotes();
+    cleanForm();
+})
+
+titleNote.addEventListener('click', showDate);
