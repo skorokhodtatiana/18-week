06a61d5d4f26b661728a507dc1680e71614993b7
skorokhodtatiana <tanjakharza@mail.ru>
2021-12-17 11:33:57 +0300

    improve code

--- 1-task-validation-html5/validationHTML5.js
+++ 1-task-validation-html5/validationHTML5.js
@@ -2,4 +2,8 @@
     let buttonSubmit = document.getElementById('buttonSubmit');
     buttonSubmit.addEventListener('submit', function preventSubmit(event){
         event.preventDefault();
+        fetch('https://httpbin.org/post', {
+            method: 'POST'
+            
+        })
     })
--- 2-task-validation-JS/1-variant/valid-js.js
+++ 2-task-validation-JS/1-variant/valid-js.js
@@ -1,53 +1,53 @@
-const errorMessage = document.getElementById('errorMessage');
-const userForm = document.getElementById('userForm');
-let inputs = document.querySelectorAll('input');
+const errorMessage = document.getElementById("errorMessage");
+const userForm = document.getElementById("userForm");
+let inputs = document.querySelectorAll("input");
 let errorArr = [];
 
 function checkInputValidity(input) {
-    let validity = input.validity;
-    if (validity.patternMismatch) {
-        errorArr.push('Неправильный формат ввода' + '\n' + input.name);
-    }
+  let validity = input.validity;
+  if (validity.patternMismatch) {
+    errorArr.push("Неправильный формат ввода" + "\n" + input.name);
+  }
 
-    if (validity.valueMissing) {
-        errorArr.push('Заполните, пожалуйста, поле' + '\n' + input.name);
-    }
+  if (validity.valueMissing) {
+    errorArr.push("Заполните, пожалуйста, поле" + "\n" + input.name);
+  }
 
-    if (validity.rangeUnderflow) {
-        errorArr.push('Слишком мало символов' + '\n' + 'в' + '\n' + input.name);
-    }
+  if (validity.rangeUnderflow) {
+    errorArr.push("Слишком мало символов" + "\n" + "в" + "\n" + input.name);
+  }
 
-    if (validity.rangeOverflow) {
-        errorArr.push('Слишком много символов' + '\n' + 'в' + '\n' + input.name);
-    }
+  if (validity.rangeOverflow) {
+    errorArr.push("Слишком много символов" + "\n" + "в" + "\n" + input.name);
+  }
 
-    if (validity.tooLong) {
-        errorArr.push('Значение' + '\n' + input.name + '\n' + 'слишком длинное');
-    }
+  if (validity.tooLong) {
+    errorArr.push("Значение" + "\n" + input.name + "\n" + "слишком длинное");
+  }
 
-    if (validity.tooShort) {
-        errorArr.push('Значение' + '\n' + input.name + '\n' + 'слишком короткое');
-    }
+  if (validity.tooShort) {
+    errorArr.push("Значение" + "\n" + input.name + "\n" + "слишком короткое");
+  }
 }
 
 function checkInputs() {
-     errorArr = [];
-     errorMessage.innerHTML = "";
+  errorArr = [];
+  errorMessage.innerHTML = "";
 
-    for (input of inputs) {
-        checkInputValidity(input);
-    }
-    errorMessage.innerHTML = errorArr.join('<br>')
+  for (input of inputs) {
+    checkInputValidity(input);
+  }
+  errorMessage.innerHTML = errorArr.join("<br>");
 }
 
-function cleanErrorMessage(){
-    errorMessage.innerHTML ="";
+function cleanErrorMessage() {
+  errorMessage.innerHTML = "";
 }
 
-userForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+userForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    checkInputs();
+  checkInputs();
 });
 
-userForm.addEventListener('change', cleanErrorMessage);
+userForm.addEventListener("change", cleanErrorMessage);
